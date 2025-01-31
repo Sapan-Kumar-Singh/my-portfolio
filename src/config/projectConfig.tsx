469579eaf4edcb6d9af27a313ac3168d89163ac8
SAPAN KUMAR <126790776+Sapan-Kumar-Singh@users.noreply.github.com>
2025-01-31 14:20:39 +0530

    Update projectConfig.tsx

--- src/config/projectConfig.tsx
+++ src/config/projectConfig.tsx
@@ -2,12 +2,5 @@
 export const projectConfig:ProjectsConfig={
     title: "Our Projects",
     subtitle: 'Transforming ideas into impactful digital solutions. Check out my latest project, where creativity meets technology! ',
-    projDesc: [{
-        imgName:"acom.png"
-    },{
-        imgName:"pace.png",
-        projLink:"https://devsaas.frontrol.com/login"
-    },{
-        imgName:"rxApmt.png"
-    }]
+   
 }
